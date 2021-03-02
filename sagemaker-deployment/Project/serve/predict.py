54739835ef0d7d0ee223738fa190928615a2aae6
EC2 Default User <ec2-user@ip-172-16-97-27.ec2.internal>
2021-03-02 05:04:03 +0000

    finished first project

--- sagemaker-deployment/Project/serve/predict.py
+++ sagemaker-deployment/Project/serve/predict.py
@@ -69,7 +69,7 @@ def predict_fn(input_data, model):
     #       You should produce two variables:
     #         data_X   - A sequence of length 500 which represents the converted review
     #         data_len - The length of the review
-    data_X, data_len = convert_and_pad_data(word_dict, revew_to_words(test_review))
+    data_X, data_len = convert_and_pad(model.word_dict, review_to_words(input_data))
 
     # Using data_X and data_len we construct an appropriate input tensor. Remember
     # that our model expects input data of the form 'len, review[500]'.
@@ -84,7 +84,6 @@ def predict_fn(input_data, model):
 
     # TODO: Compute the result of applying the model to the input data. The variable `result` should
     #       be a numpy array which contains a single integer which is either 1 or 0
-
-    result = model(data)
+    result = np.asarray(model(data).detach().numpy().round().astype('int'))
 
     return result
